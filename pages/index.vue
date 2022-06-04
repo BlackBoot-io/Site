d7a3780c0aee50e38fe99d45c3d5906f6c66b3c2
Roozbeh Eslami <r.eslami@asax.local>
2022-06-04 21:08:02 +0430

    add layout and project structure

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col class="text-center">
+      Index Page
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
